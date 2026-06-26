dae51ccb3c61cd10f85f7b2823788cba74863dc9
rohan-p8 <rohanpatil0449@gmail.com>
2026-06-26 22:57:04 +0530

    check content in files

---
+++ Practice Set 9/problem_9.py
@@ -0,0 +1,12 @@
+
+with open("this.txt", "r") as f:
+    content = f.read()
+
+with open("copy.txt", "r") as f:
+    content_c = f.read()
+
+    if content == content_c:
+        print("The content of both files is the same.")
+    else:
+        print("The content of both files is different.")
+
