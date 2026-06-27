e8d53bcf28f3c8f5ebfa12e9c28b385cb70247aa
rohan-p8 <rohanpatil0449@gmail.com>
2026-06-27 23:27:26 +0530

    update file

---
+++ Practice Set 9/update_file.py
@@ -0,0 +1,24 @@
+def game():
+    return 95
+
+score = game()
+
+with open("Hi_score.txt", "r") as f:
+    hi_score = f.read()
+
+if hi_score == "":
+    hi_score = 0
+else:
+    hi_score = int(hi_score)
+
+if score > hi_score:
+    with open("Hi_score.txt", "w") as f:
+        f.write(str(score))
+
+
+with open("Hi_score.txt", "r") as f:
+    content = f.read()
+
+print(content)
+
+game()
