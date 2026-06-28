b5a01b757c1f8ec3fa5893de1d9ff151d4ac012e
rohan-p8 <rohanpatil0449@gmail.com>
2026-06-28 23:23:31 +0530

    create problem_3.py
    perform operations in class

---
+++ Practice Set 10/problem_3.py
@@ -0,0 +1,13 @@
+
+class Sample:
+    a = 10
+
+obj = Sample()
+
+print("Class attribute before:", Sample.a)
+print("Object attribute before:", obj.a)
+
+obj.a = 20
+
+print("Class attribute after:", Sample.a)
+print("Object attribute after:", obj.a)
