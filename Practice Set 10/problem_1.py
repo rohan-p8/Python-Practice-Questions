4703eef1a639ebca3d12d92c22e485cb68e62c80
rohan-p8 <rohanpatil0449@gmail.com>
2026-06-28 23:20:21 +0530

    first problem
    create class, constructor and method

---
+++ Practice Set 10/problem_1.py
@@ -0,0 +1,23 @@
+class Programmer:
+    company = "Microsoft"
+
+    def __init__(self, name, empid, salary):
+        self.name = name
+        self.empid = empid
+        self.salary = salary
+
+
+    def getInfo(self):
+        print(f"Company: {self.company}")
+        print(f"Name: {self.name}")
+        print(f"Employee ID: {self.empid}")
+        print(f"Salary: {self.salary}")
+
+
+p1 = Programmer("Rohan", 101, 50000)
+
+p2 = Programmer("Sohan", 102, 60000)
+
+p1.getInfo()
+print()
+p2.getInfo()
