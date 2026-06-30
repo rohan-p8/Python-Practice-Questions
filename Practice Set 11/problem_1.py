a625bf4f803885f96eb2a4116992aca848acee00
rohan-p8 <rohanpatil0449@gmail.com>
2026-06-30 15:27:38 +0530

    vector problem

---
+++ Practice Set 11/problem_1.py
@@ -0,0 +1,29 @@
+
+class TwoDVector:
+
+    def __init__(self, i, j):
+        self.i = i
+        self.j = j
+
+    def show(self):
+        print(f"TwoDVector: i={self.i}, j={self.j}")
+
+
+
+class ThreeDVector(TwoDVector):
+
+    def __init__(self, i, j, k):
+        super().__init__(i, j)
+        self.k = k
+
+    def show(self):
+        print(f"ThreeDVector: i={self.i}, j={self.j}, k={self.k}")
+
+
+
+
+a = TwoDVector(3,5)
+a.show()
+
+b = ThreeDVector(8, 5,1)
+b.show()
