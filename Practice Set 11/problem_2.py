68cdd0feb23cecb05be034107b003ac73dbecd8b
rohan-p8 <rohanpatil0449@gmail.com>
2026-06-30 15:29:21 +0530

    create problem_2.py file

---
+++ Practice Set 11/problem_2.py
@@ -0,0 +1,29 @@
+class Animals:
+
+    def __init__(self, name):
+        self.name = name
+        print(f"Animal name: {self.name}")
+
+
+
+class Pets(Animals):
+
+    def __init__(self, name, belong):
+        super().__init__(name)
+        self.belong = belong
+        print(f"Belong to: {self.belong}")
+
+class Dog(Pets):
+
+    def __init__(self, name, belong):
+        super().__init__(name, belong)
+
+    
+    def bark(self):
+        print(f"{self.name} is barking!")
+
+
+p = Pets("Tokyo", "Pets family")
+d = Dog("Motya", "Pets family")
+
+d.bark()
