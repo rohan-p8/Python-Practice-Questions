462da84eb3e8016b31be2fdfc14bee579cf3b9b5
rohan-p8 <rohanpatil0449@gmail.com>
2026-07-02 23:12:42 +0530

    create problem_1.py
    use try-except block to handle error

---
+++ Practice Set 12/problem_1.py
@@ -0,0 +1,16 @@
+
+try:
+    with open("1.txt", "r") as f1, open("2.txt", "r") as f2, open("3.txt", "r") as f3:
+        print(f1.read())
+        print(f2.read())
+        print(f3.read())
+
+except:
+    print("Files not found")
+
+
+print("This is executed because we have handled the exception")
+
+
+# with open("2.txt", "r") as f2:
+#     print(f2.read())
