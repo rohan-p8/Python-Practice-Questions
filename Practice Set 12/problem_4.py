22b53ed898b306e3eea61b10973f730adcfea95e
rohan-p8 <rohanpatil0449@gmail.com>
2026-07-03 20:52:01 +0530

    create problem_4.py
    generate mul table using list comp and store in file

---
+++ Practice Set 12/problem_4.py
@@ -0,0 +1,19 @@
+
+try:
+
+    a = int(input("Enter 1st number: "))
+    b = int(input("Enter 2nd number: "))
+
+    result = a / b
+    print("The result of division is:", result)
+
+
+except ZeroDivisionError:
+    print("Infinite by handling the 'ZeroDivisionError' exception")
+
+else:
+    print("This block is executed only if no exception occurs.")
+
+
+finally:
+    print("This block is always executed, regardless of whether an exception occurred or not.")
