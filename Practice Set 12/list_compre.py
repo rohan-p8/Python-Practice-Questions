6ebae996eade3a639db5db91d618c8ecbb0e3431
rohan-p8 <rohanpatil0449@gmail.com>
2026-07-03 20:54:03 +0530

    list_comp.py
    Use list comp to print squares

---
+++ Practice Set 12/list_compre.py
@@ -0,0 +1,14 @@
+# Without list comprehension
+
+# l = []
+
+# for i in range (1, 11):
+#     l.append(i)
+
+# print(l)
+
+
+# With list comprehension
+
+numbers = [i*i for i in range(1, 11)]
+print(numbers)
