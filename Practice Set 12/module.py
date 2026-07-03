1b02e4b54956e60d9a15eefd90fbddc25556de9c
rohan-p8 <rohanpatil0449@gmail.com>
2026-07-03 20:55:41 +0530

    module.py
    if __name__ == "__main__"
    module program that define how to import module use of main line

---
+++ Practice Set 12/module.py
@@ -0,0 +1,11 @@
+def greet():
+    print("Hello! Welcome to the program.")
+
+
+print(__name__)
+
+if __name__ == "__main__":
+    print("This code is running as the main program.")
+    greet()
+    print(__name__)
+    
