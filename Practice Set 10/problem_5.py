3becadb3fae8a2edccd4884ed28919310d4c05d7
rohan-p8 <rohanpatil0449@gmail.com>
2026-07-06 22:20:05 +0530

    Update problem_5.py

--- Practice Set 10/problem_5.py
+++ Practice Set 10/problem_5.py
@@ -3,18 +3,14 @@ class Train:
     def __init__(self, train_number):
         self.train_number = train_number
 
-
     def book_ticket(self, frm, to):
         print(f"Your ticket for train {self.train_number} from {frm} to {to} has been booked successfully.")
-
     
     def get_status(self):
         print("Your train is on time.")
-
     
     def fare_info(self, frm, to):
         print(f"The fare for the train from {frm} to {to} is $50.")
-
 
 t1 = Train("12345")
 t1.book_ticket("New York", "Los Angeles")
