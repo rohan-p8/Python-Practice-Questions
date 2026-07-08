8157d88d3f741815867f979a50864146d24bdc35
rohan-p8 <rohanpatil0449@gmail.com>
2026-07-08 11:07:06 +0530

    Update tables.py

--- Practice Set 9/tables.py
+++ Practice Set 9/tables.py
@@ -1,5 +1,4 @@
 for i in range(2, 21):
     with open(f"tables/table_{i}.txt", "w") as file:
         for j in range(1, 11):
-            file.write(f"{i} x {j} = {i * j}\n")
-            
+            file.write(f"{i} x {j} = {i * j}\n")     
