8b02cb087e07e5a52c6dbb02b74e02550e283d44
rohan-p8 <rohanpatil0449@gmail.com>
2026-08-01 22:24:03 +0530

    Update problem_6.py

--- Practice Set 10/problem_6.py
+++ Practice Set 10/problem_6.py
@@ -1,7 +1,12 @@
 class Programmer:
     company = "Microsoft"
 
-# You can also use any valid variable name in place of 'self' but it is a convention to use 'self' as the first parameter of instance methods in Python. It refers to the instance of the class and allows you to access the attributes and methods of that instance.   
+# You can also use any valid variable name in place of 'self' but 
+# it is a convention to use 'self' as the first parameter of 
+# instance methods in Python. It refers to the instance of the 
+# class and allows you to access the attributes and methods of 
+# that instance.   
+
     def __init__(self, name, empid, salary):
         self.name = name
         self.empid = empid
