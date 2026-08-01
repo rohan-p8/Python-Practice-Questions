fcb0f6acf30e7f9e4972a4edc22b88d3568ef5b3
rohan-p8 <rohanpatil0449@gmail.com>
2026-08-01 22:21:50 +0530

    Update problem_6.py

--- Practice Set 10/problem_6.py
+++ Practice Set 10/problem_6.py
@@ -1,18 +1,17 @@
 class Programmer:
     company = "Microsoft"
 
-
 # You can also use any valid variable name in place of 'self' but it is a convention to use 'self' as the first parameter of instance methods in Python. It refers to the instance of the class and allows you to access the attributes and methods of that instance.   
-    def __init__(slf, name, empid, salary):
-        slf.name = name
-        slf.empid = empid
-        slf.salary = salary
+    def __init__(self, name, empid, salary):
+        self.name = name
+        self.empid = empid
+        self.salary = salary
 
-    def getInfo(slf):
-        print(f"Company: {slf.company}")
-        print(f"Name: {slf.name}")
-        print(f"Employee ID: {slf.empid}")
-        print(f"Salary: {slf.salary}")
+    def getInfo(self):
+        print(f"Company: {self.company}")
+        print(f"Name: {self.name}")
+        print(f"Employee ID: {self.empid}")
+        print(f"Salary: {self.salary}")
 
 p1 = Programmer("Rohan", 101, 50000)
 
