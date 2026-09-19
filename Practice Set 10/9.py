12a5edb2e0cb3c8cbfd58b0e45ac4d0fbf56d079
rohan-p8 <rohanpatil0449@gmail.com>
2026-09-19 23:15:36 +0530

    Update 9.py

---
+++ Practice Set 10/9.py
@@ -0,0 +1,12 @@
+# Take a character and check if it is a letter, a digit, or neither.
+
+char = input("Enter character: ")
+
+if char.isalpha():
+    print("Letter")
+
+elif char.isdigit():
+    print("Digit")
+
+else:
+    print("Neither letter or digit")
