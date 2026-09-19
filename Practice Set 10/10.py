5ef21fe964e60cebe02aa362357d276d94c97f5a
rohan-p8 <rohanpatil0449@gmail.com>
2026-09-19 23:19:45 +0530

    Create 10.py

---
+++ Practice Set 10/10.py
@@ -0,0 +1,13 @@
+# Take a number and print “Fizz” if divisible by 3, “Buzz” if divisible by 5, and 
+# “FizzBuzz” if divisible by both. 
+
+n = int(input("Enter a number: "))
+
+if n % 3 == 0:
+    print("Fizz")
+
+elif n % 5 == 0:
+    print("Buzz")
+
+elif n % 3 == 0 and n % 5 == 0:
+    print("FizzBuzz")
