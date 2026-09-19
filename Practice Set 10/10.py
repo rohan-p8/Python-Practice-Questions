705441f6f3f2c5f1f09d43883715ef7daf2f26be
rohan-p8 <rohanpatil0449@gmail.com>
2026-09-19 23:21:12 +0530

    Update 10.py

--- Practice Set 10/10.py
+++ Practice Set 10/10.py
@@ -3,11 +3,11 @@
 
 n = int(input("Enter a number: "))
 
-if n % 3 == 0:
-    print("Fizz")
+if n % 3 == 0 and n % 5 == 0:
+    print("FizzBuzz")
 
 elif n % 5 == 0:
     print("Buzz")
 
-elif n % 3 == 0 and n % 5 == 0:
-    print("FizzBuzz")
+elif n % 3 == 0:
+    print("Fizz")
